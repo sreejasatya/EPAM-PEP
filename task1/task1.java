68eb86162b19c54e0adb8fb740f4fb20f24eec27
sreejasatya <sreejasatya14@gmail.com>
2019-03-26 21:12:52 +0530

    Add files via upload

---
+++ task1/task1.java
@@ -0,0 +1,17 @@
+package task1;
+
+/**
+ * 
+ * @author Vaishnavi
+ * Hello World!  
+ *
+ */
+public class task1 {
+
+	public static void main(String[] args) {
+		
+       System.out.println("Hello World!");
+       
+	}
+
+}
